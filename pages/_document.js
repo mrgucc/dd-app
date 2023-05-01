51817150c5ed048a8c8dbfb49ad1552434fb1def
Matt G <66087058+mrgucc@users.noreply.github.com>
2023-05-01 14:39:48 -0400

    init commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable @next/next/no-head-import-in-document */
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Head from 'next/head';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <>
+                <Head>
+                  <link rel='preconnect' href='https://fonts.googleapis.com'></link>
+                  <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin></link>
+                  <link
+                    href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'
+                    rel='stylesheet'
+                  ></link>
+                </Head>
+                <App {...props} />
+              </>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
